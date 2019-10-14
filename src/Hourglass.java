133488c70a1abf820ab2cb33556a0ead0abb7921
postunat000 <postunat000@CAM-LAB-08.bgsd.k12.wa.us>
2019-10-14 07:56:20 -0700

    yay.java

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -31,13 +31,17 @@ public class Hourglass {
 	public static void topHourglass() {
 		//First come up with a loop for the spaces before the body
 		//This loop determines how many lines there are
-		for (int i = 1; i <= 4; i++) {
-			//This loop defines the loop for spaces before the body
-			for (int j = 1; j <= i; j++) {
-			System.out.print(" ");
+		for (int line = 1; line <= 4; line++) {
+			int colons = -2 * line + 10;
+			int spaces = 1 * line + 0;
+			for (int i = 1; i <= spaces; i++) {
+				System.out.print(" ");
+				//This loop defines the loop for spaces before the body
 			}
-			System.out.print("\\");
-			topInside();
+			
+			for (int j = 1; j <= i; j++) {
+				
+			}
 		}
 	}
 	
@@ -54,6 +58,7 @@ public class Hourglass {
 	}
 	
 	public static void topInside() {
+		int colons = -2 * line + 10;
 		for (int i = 1; i <= 8; i++) { //The i <= determines the number of colons
 			for (int j = 8; j >= 2; j--);
 			System.out.print(":");
